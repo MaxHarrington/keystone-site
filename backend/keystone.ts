91b27ec9c3518446ff42d4982fb720a6625e89e8
Maxine <max.harrington@mail.mcgill.ca>
2026-02-01 04:32:09 -0500

    Restoring poster functionality on session creation

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -13,35 +13,9 @@ export default config<TypeInfo>({
         provider: 'postgresql', url: String(process.env.DATABASE_URL), prismaClientPath: './node_modules/.prisma/client'
     }, lists, ui: {
         isAccessAllowed: async (context) => {
-            return sessionIsPoster({
-                session: await session.get({context})
-                    ?? await session.start({context})
-                    ?? undefined
-            });
+            return sessionIsPoster({ session: context.session });
         }, pageMiddleware: async ({wasAccessAllowed, context}) => {
-            // const url = new URL(`${keystone}${context.req?.url}`);
-            // const hadCode = url.searchParams.has('code')
-            //     || url.searchParams.has('session_state')
-            //     || url.searchParams.has('iss');
-            // if (hadCode) {
-            //     url.searchParams.delete('code');
-            //     url.searchParams.delete('session_state');
-            //     url.searchParams.delete('iss');
-            // }
-            // if (!wasAccessAllowed) {
-            //     logger.child({
-            //         wasAccessAllowed, hadCode, session: context.session
-            //     }).warn('Access denied');
-            //     if (context.session) {
-            //         return {
-            //             kind: 'redirect', to: frontend
-            //         };
-            //     } else return {
-            //         kind: 'redirect', to: `${login}${keystone}${url.pathname}`
-            //     };
-            // } else if (hadCode) return {
-            //     kind: 'redirect', to: `${url.pathname}${url.search}`
-            // }
+
         },
     }, session, storage: {
         local_images: {
--- backend/lib/auth.ts
+++ backend/lib/auth.ts
@@ -1,38 +1,19 @@
 import slugify from 'slugify';
 import type {Context} from '.keystone/types';
 import {User} from '@prisma/client';
-import {logger} from './logger';
-import {parse, serialize} from "cookie";
 
 export const keycloakSessionStrategy = {
     get: async function ({context}: { context: Context }): Promise<User | null> {
         if (!context.req || !context.res) return null;
         if (context.req.headers.authorization === 'anonymous') return null;
         const id = context.req?.headers['x-auth-request-user'];
-        const groups = context.req.headers['x-auth-request-groups'];
-        if (typeof id != 'string' || typeof groups != 'string') return null;
-        const split = groups.split(',')
-        const admin = split.includes('role:admin');
-        const poster = split.includes('role:poster');
-        const community = split.includes('role:community');
+        if (typeof id != 'string') return null;
         const user = await context.sudo().db.User.findOne({
             where: {
                 id
             }
         });
         if (!user) return null;
-        if (admin != user.admin || poster != user.poster || community != user.community) {
-            context.sudo().db.User.updateOne({
-                where: {
-                    id
-                },
-                data: {
-                    admin,
-                    community,
-                    poster
-                }
-            })
-        }
         return user;
     },
 
@@ -46,7 +27,7 @@ export const keycloakSessionStrategy = {
         const admin = split.includes('role:admin');
         const poster = split.includes('role:poster');
         const community = split.includes('role:community');
-        const user = await context.db.User.findOne({
+        const user = await context.sudo().db.User.findOne({
             where: {
                 id,
             }
@@ -60,7 +41,32 @@ export const keycloakSessionStrategy = {
                     poster,
                     community,
                 }
-            })
+            });
+        } else if (admin != user.admin || poster != user.poster || community != user.community) {
+            context.sudo().db.User.updateOne({
+                where: {
+                    id
+                },
+                data: {
+                    admin,
+                    community,
+                    poster
+                }
+            });
+        }
+        if (poster) {
+            const posterObject = await context.sudo().db.Poster.findOne({
+                where: {
+                    id
+                }
+            });
+            if (!posterObject) {
+                await context.sudo().prisma.poster.create({
+                    data: {
+                        id
+                    }
+                });
+            }
         }
         return user;
     },
